e12e91a46d83217af1d2d36952a149135336daba
Hien Huynh <hien.huynh@gogame.net>
2018-10-17 16:12:14 +0700

    add

--- app/app.js
+++ app/app.js
@@ -19,16 +19,32 @@ const app = new App(config);
 
 var request = require("request");
 
-var options = { method: 'POST',
-  url: 'https://fcm.googleapis.com/fcm/send',
+// var options = { method: 'POST',
+//   url: 'https://fcm.googleapis.com/fcm/send',
+//   headers: 
+//    { 'postman-token': '2de21482-da45-44b2-8641-9a201d77562c',
+//      'cache-control': 'no-cache',
+//      'content-type': 'application/json',
+//      authorization: 'key=AAAA87ESbJk:APA91bE3bZqoVtdk-KB0pIvt9DboHSYqKvCuj41-QOdlQC3oIpsyJ90X70kkJVSsKDHS4lki7uuPvgl66tXrVCgv-wYdikRk7e__9xkX6WxuvP1prO4qAG2cMHB2K7nNll_Wef-P8-sZ' },
+//   body: 
+//    { notification: { title: 'Hello', body: 'World', icon: 'firebase-icon.png' },
+//      to: 'cQpFHcbb_No:APA91bF9jYYO15dZHJW3FHqOG7SAha0xlDjDDck9wi1NMOY1lZ6eNYDpI7jlFlSfyz_Z5YGXPzbNdKZ6qBiNKrhK7sLhEBOrZee07isoD1LbVUcMaNVjmaryp_mWzpX_hmqrzonVuHyp' },
+//   json: true };
+
+  var options = { method: 'POST',
+  url: 'https://api.amazon.com/messaging/registrations/amzn1.adm-registration.v3.Y29tLmFtYXpvbi5EZXZpY2VNZXNzYWdpbmcuUmVnaXN0cmF0aW9uSWRFbmNyeXB0aW9uS2V5ITEhU2YzSHpBRlR2OElERnFCaWd0YVF0dWRzSTlnRU8xQjM3MGlOVFdEVkUyVS91UjFwcklRVTRDR3JWOEtoeGlDOXcrTEd4aCtpSm9neUJDMmVPK0pBWFB1OXZqK1M5cGhndU43TEtyMFJ6QlVxaDd3MEhKNSswdlgvTWtqeDFuQTZKVUswWUFvZXZHVzFEcWdHcWdEVVZYNGNSVkcrZVVNQStBZEZkSkpJb1Jtc0Fsa2hZRnRTT0ppenR1SFo5QUtmaW44c2dnZDlxQk1HZ1RsUVZYcDhnQkR5N20yakx2ZlhZdlhZSE9aSThjblArQzJDNGRrUFJVWkVNRFN1YkVLMnB4RTYyOFN4dEdNWUhxc2pJbTdSMm1jM1pWRzJaYmkwWWxtTldBajEvbTloRXVkY2poVUVnMG4yQnJmTHBoSnF1M0NBYURqcE85Z1doOWtTM1lOY2lMaCtuNkJrRGZLRTdYaTlheHIxTndZPSEwcFNvNUE1YklySi9TTUNkUEhLN3ZnPT0/messages',
   headers: 
-   { 'postman-token': '2de21482-da45-44b2-8641-9a201d77562c',
+   { 'postman-token': '841ae4a1-a280-80f0-0de5-3c6e40cdd050',
      'cache-control': 'no-cache',
-     'content-type': 'application/json',
-     authorization: 'key=AAAA87ESbJk:APA91bE3bZqoVtdk-KB0pIvt9DboHSYqKvCuj41-QOdlQC3oIpsyJ90X70kkJVSsKDHS4lki7uuPvgl66tXrVCgv-wYdikRk7e__9xkX6WxuvP1prO4qAG2cMHB2K7nNll_Wef-P8-sZ' },
+     authorization: 'Bearer Atc|MQEBIGZUnyi3jZ2zX8-9dWUHjj5O10b5vWcIEsejXpN4MqkdoBOnJwFZa9h1wovCWP_MTg_4q6gGSE48sPOaJozsy0kSqLK_y-xSSSwtJoYQjD6BnRQ-tY4Y4Nd7ZdERPmPfTu_2gsqxTCJx7uM_bsv9IEe6QuwcXwPVk2Af9EvM3RtaLmwWuCvIwkW3M68SJLfOIwtbkEQLLgWIqmQrtzVQbCRbKjYt2eAF1txqGw1HnDX7ITGAxuwdQrMtPkDDPkxk86N4Pvz9b_uqTlVlVJ8xPbHp',
+     'x-amzn-accept-type': 'com.amazon.device.messaging.ADMSendResult@1.0',
+     accept: 'application/json',
+     'x-amzn-type-version': 'com.amazon.device.messaging.ADMMessage@1.0',
+     'content-type': 'application/json' },
   body: 
-   { notification: { title: 'Hello', body: 'World', icon: 'firebase-icon.png' },
-     to: 'cQpFHcbb_No:APA91bF9jYYO15dZHJW3FHqOG7SAha0xlDjDDck9wi1NMOY1lZ6eNYDpI7jlFlSfyz_Z5YGXPzbNdKZ6qBiNKrhK7sLhEBOrZee07isoD1LbVUcMaNVjmaryp_mWzpX_hmqrzonVuHyp' },
+   { data: { message: 'value1', timeStamp: 'value2' },
+     consolidationKey: 'Some Key',
+     expiresAfter: 86400 },
   json: true };
 
 //   const Alexa = require('ask-sdk-core');
