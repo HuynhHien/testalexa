652567dab69552c92bba2bde5830e3d3a066d423
Hien Huynh <hien.huynh@gogame.net>
2018-10-17 15:47:23 +0700

    test custom function

--- app/app.js
+++ app/app.js
@@ -31,85 +31,86 @@ var options = { method: 'POST',
      to: 'cQpFHcbb_No:APA91bF9jYYO15dZHJW3FHqOG7SAha0xlDjDDck9wi1NMOY1lZ6eNYDpI7jlFlSfyz_Z5YGXPzbNdKZ6qBiNKrhK7sLhEBOrZee07isoD1LbVUcMaNVjmaryp_mWzpX_hmqrzonVuHyp' },
   json: true };
 
-  const Alexa = require('ask-sdk-core');
+//   const Alexa = require('ask-sdk-core');
 
-const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
-        console.log('Welcome to the Alexa Skills Kit, you can say hello!');
-        return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .withSimpleCard('Hello World', speechText)
-        .getResponse();
-    },
-};
+// const LaunchRequestHandler = {
+//     canHandle(handlerInput) {
+//         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+//     },
+//     handle(handlerInput) {
+//         const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+//         console.log('Welcome to the Alexa Skills Kit, you can say hello!');
+//         return handlerInput.responseBuilder
+//         .speak(speechText)
+//         .reprompt(speechText)
+//         .withSimpleCard('Hello World', speechText)
+//         .getResponse();
+//     },
+// };
 
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speechText = 'You have just unlock secret mode in Sonic! Enjoy!';
+// const HelloWorldIntentHandler = {
+//     canHandle(handlerInput) {
+//         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+//         && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+//     },
+//     handle(handlerInput) {
+//         const speechText = 'You have just unlock secret mode in Sonic! Enjoy!';
 
-        /* request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+//         /* request(options, function (error, response, body) {
+//         if (error) throw new Error(error);
         
-        console.log(body);
-        });*/
+//         console.log(body);
+//         });*/
 
-        return handlerInput.responseBuilder
-        .speak(speechText)
-        .withSimpleCard('Hello World', speechText)
-        .getResponse();
+//         return handlerInput.responseBuilder
+//         .speak(speechText)
+//         .withSimpleCard('Hello World', speechText)
+//         .getResponse();
+//     },
+// };
+
+// const ErrorHandler = {
+//   canHandle() {
+//     return true;
+//   },
+//   handle(handlerInput, error) {
+//     console.log(`Error handled: ${error.message}`);
+
+//     return handlerInput.responseBuilder
+//       .speak('Sorry, I can\'t understand the command. Please say again.')
+//       .reprompt('Sorry, I can\'t understand the command. Please say again.')
+//       .getResponse();
+//   },
+// };
+
+// const skillBuilder = Alexa.SkillBuilders.custom();
+
+// exports.handler = skillBuilder
+//   .addRequestHandlers(
+//     LaunchRequestHandler,
+//     HelloWorldIntentHandler
+//   )
+//   .addErrorHandlers(ErrorHandler)
+//   .lambda();
+
+//   app.setHandler(exports.handler);
+app.setHandler({
+    'LAUNCH': function() {
+        this.toIntent('HelloWorldIntent');
     },
-};
 
-const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
+    'HelloWorldIntent': function() {
+        // request(options, function (error, response, body) {
+        //     if (error) throw new Error(error);    
+        //         console.log(body);
+        // });
+        this.customPushNotify(options);
+        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+    },
 
-    return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
-      .getResponse();
-  },
-};
-
-const skillBuilder = Alexa.SkillBuilders.custom();
-
-exports.handler = skillBuilder
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    HelloWorldIntentHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
-
-  app.setHandler(exports.handler);
-// app.setHandler({
-//     'LAUNCH': function() {
-//         this.toIntent('HelloWorldIntent');
-//     },
-
-//     'HelloWorldIntent': function() {
-//         // request(options, function (error, response, body) {
-//         //     if (error) throw new Error(error);    
-//         //         console.log(body);
-//         // });
-//         this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-//     },
-
-//     'MyNameIsIntent': function(name) {
-//         this.tell('Hey ' + name.value + ', nice to meet you!');
-//     },
-// });
+    'MyNameIsIntent': function(name) {
+        this.tell('Hey ' + name.value + ', nice to meet you!');
+    },
+});
 
 module.exports.app = app;
