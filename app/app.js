b297c1ba0de4f80403897fb2b5f416258f22ece2
Hien Huynh <hien.huynh@gogame.net>
2018-10-17 15:24:22 +0700

    test

--- app/app.js
+++ app/app.js
@@ -17,18 +17,38 @@ const app = new App(config);
 // App Logic
 // =================================================================================
 
-// app.setHandler({
-//     'LAUNCH': function() {
-//         this.toIntent('HelloWorldIntent');
-//     },
+var request = require("request");
 
-//     // 'HelloWorldIntent': function() {
-//     //     this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-//     // },
+var options = { method: 'POST',
+  url: 'https://fcm.googleapis.com/fcm/send',
+  headers: 
+   { 'postman-token': '2de21482-da45-44b2-8641-9a201d77562c',
+     'cache-control': 'no-cache',
+     'content-type': 'application/json',
+     authorization: 'key=AAAA87ESbJk:APA91bE3bZqoVtdk-KB0pIvt9DboHSYqKvCuj41-QOdlQC3oIpsyJ90X70kkJVSsKDHS4lki7uuPvgl66tXrVCgv-wYdikRk7e__9xkX6WxuvP1prO4qAG2cMHB2K7nNll_Wef-P8-sZ' },
+  body: 
+   { notification: { title: 'Hello', body: 'World', icon: 'firebase-icon.png' },
+     to: 'cQpFHcbb_No:APA91bF9jYYO15dZHJW3FHqOG7SAha0xlDjDDck9wi1NMOY1lZ6eNYDpI7jlFlSfyz_Z5YGXPzbNdKZ6qBiNKrhK7sLhEBOrZee07isoD1LbVUcMaNVjmaryp_mWzpX_hmqrzonVuHyp' },
+  json: true };
 
-//     'MyNameIsIntent': function(name) {
-//         this.tell('Hey ' + name.value + ', nice to meet you!');
-//     },
-// });
+
+
+app.setHandler({
+    'LAUNCH': function() {
+        this.toIntent('HelloWorldIntent');
+    },
+
+    'HelloWorldIntent': function() {
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);    
+                console.log(body);
+        });
+        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+    },
+
+    'MyNameIsIntent': function(name) {
+        this.tell('Hey ' + name.value + ', nice to meet you!');
+    },
+});
 
 module.exports.app = app;
--- index.js
+++ index.js
@@ -3,23 +3,13 @@
 
 const Alexa = require('ask-sdk-core');
 
-var request = require("request");
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const {app} = require('./app/app.js');
 
-var options = { method: 'POST',
-  url: 'https://fcm.googleapis.com/fcm/send',
-  headers: 
-   { 'postman-token': '2de21482-da45-44b2-8641-9a201d77562c',
-     'cache-control': 'no-cache',
-     'content-type': 'application/json',
-     authorization: 'key=AAAA87ESbJk:APA91bE3bZqoVtdk-KB0pIvt9DboHSYqKvCuj41-QOdlQC3oIpsyJ90X70kkJVSsKDHS4lki7uuPvgl66tXrVCgv-wYdikRk7e__9xkX6WxuvP1prO4qAG2cMHB2K7nNll_Wef-P8-sZ' },
-  body: 
-   { notification: { title: 'Hello', body: 'World', icon: 'firebase-icon.png' },
-     to: 'cQpFHcbb_No:APA91bF9jYYO15dZHJW3FHqOG7SAha0xlDjDDck9wi1NMOY1lZ6eNYDpI7jlFlSfyz_Z5YGXPzbNdKZ6qBiNKrhK7sLhEBOrZee07isoD1LbVUcMaNVjmaryp_mWzpX_hmqrzonVuHyp' },
-  json: true };
+
 
 
   const verifiedServer = express();
@@ -62,7 +52,7 @@ const LaunchRequestHandler = {
   },
   handle(handlerInput) {
     const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
-
+    console.log('Welcome to the Alexa Skills Kit, you can say hello!');
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
